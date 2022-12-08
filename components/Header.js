6d52283de1abdd1be977c6e8150df3d4c592434b
eric polley <ericpolley@live.com>
2022-12-07 16:35:17 -0800

    margin fix.

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,12 @@ export default function Header() {
   return (
     <header className='flex justify-between items-center p-5 text-sm text-gray-700'>
        <div className='flex space-x-4 item-center '>
-        <p className='link'>About</p>
-        <p className='link'>Store</p>
+        <p className='link'> About </p>
+        <p className='link'> Store </p>
        </div>
        <div className='flex space-x-4 item-center justify-between'>
-        <p className='link pt-2'>Gmail</p>
-        <p className='link pt-2'>Images</p>
+        <p className='link pt-2'> Gmail </p>
+        <p className='link pt-2'> Images </p>
         <User />
        </div>
     </header>
